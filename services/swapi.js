330136f389081c9b2b5386537685f1515e0967c7
halokin <halokin@gmail.com>
2016-10-16 18:53:00 +0200

    movies informations

--- services/swapi.js
+++ services/swapi.js
@@ -1,21 +1,31 @@
 var services = angular.module('swapi', []);
 
 services.factory('SwapiService', ['$http',
-  function($http){
-    function Swapi(){}
+    function($http) {
+        function Swapi() {}
 
-    Swapi.domain = 'http://swapi.co/api';
+        Swapi.domain = 'http://swapi.co/api';
 
-    Swapi.people = function(){
-      var path = '/people';
-      var url = Swapi.domain + path;
+        Swapi.people = function() {
+            var path = '/people';
+            var url = Swapi.domain + path;
 
-      return $http.get(url)
-        .then(function(response){
-          return response;
-        });
-    };
+            return $http.get(url)
+                .then(function(response) {
+                    return response;
+                });
+        };
 
-    return Swapi;
-  }
+        Swapi.films = function() {
+            var path = '/films';
+            var url = Swapi.domain + path;
+
+            return $http.get(url)
+                .then(function(response) {
+                    return response;
+                });
+        };
+
+        return Swapi;
+    }
 ]);
